8191d193b07c0dc279d67b0160d50c503c70c32b
Desislava Nikolova <desata@gmail.com>
2025-05-08 11:19:31 +0300

    20250508

---
+++ 20250505-20250511/14. Streams, Files and Directories/DirectoryTraversal/DirectoryTraversal.cs
@@ -0,0 +1,68 @@
+﻿namespace DirectoryTraversal
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
+        }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            Dictionary<string, List<FileInfo>> filesByExtension = new Dictionary<string, List<FileInfo>>();
+
+            DirectoryInfo dir = new DirectoryInfo(inputFolderPath);
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                string ext = file.Extension;
+
+                if (!filesByExtension.ContainsKey(ext))
+                {
+                    filesByExtension[ext] = new List<FileInfo>();
+                }
+
+                filesByExtension[ext].Add(file);
+            }
+
+            var sortedExtensions = filesByExtension
+                .OrderByDescending(e => e.Value.Count)
+                .ThenBy(e => e.Key);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var extGroup in sortedExtensions)
+            {
+                sb.AppendLine(extGroup.Key);
+
+                foreach (var file in extGroup.Value.OrderBy(f => f.Length))
+                {
+                    sb.AppendLine($"--{file.Name} - {(file.Length / 1024.0):F3}kb");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fullPath = desktopPath + reportFileName;
+
+            File.WriteAllText(fullPath, textContent);
+        }
+    }
+}
---
+++ 20250505-20250511/14. Streams, Files and Directories/ZipAndExtract/ZipAndExtract .cs
@@ -0,0 +1,44 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFile = @"..\..\..\copyMe.png";
+            string zipArchiveFile = @"..\..\..\archive.zip";
+            string extractedFile = @"..\..\..\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            using (FileStream fileToArchive = new FileStream(zipArchiveFilePath, FileMode.Create))
+            {
+                using (ZipArchive archive = new ZipArchive(fileToArchive, ZipArchiveMode.Create))
+                {
+                    archive.CreateEntryFromFile(inputFilePath, Path.GetFileName(inputFilePath));
+                }
+            }
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            using (ZipArchive archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Update))
+            {
+                ZipArchiveEntry entry = archive.GetEntry(fileName);
+                using (StreamWriter writer = new StreamWriter(entry.Open()))
+                {
+                    writer.BaseStream.Seek(0, SeekOrigin.End);
+                }
+            }
+        }
+    }
+}
