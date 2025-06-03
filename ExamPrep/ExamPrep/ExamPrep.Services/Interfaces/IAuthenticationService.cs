5dd243fca7e7ae8fcc14d541651d1231a9624853
Desislava Nikolova <desata@gmail.com>
2025-06-03 17:11:26 +0300

    remove

--- ExamPrep/ExamPrep/ExamPrep.Services/Interfaces/IAuthenticationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ExamPrep.Services.DTOs.Authentication;
-
-namespace ExamPrep.Services.Interfaces
-{
-    public interface IAuthenticationService
-    {
-        Task<LoginResponse> LoginAsync(LoginRequest request);
-    }
-}
--- ExamPrep/ExamPrep/ExamPrep.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ExamPrep.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
