8819eb323c0e6defb3facad68fe2f831bf7f77fb
Desislava Nikolova <desata@gmail.com>
2025-06-03 17:02:37 +0300

    WikiSystem Additional

---
+++ ExamPrep/ExamPrep/ExamPrep.Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using ExamPrep.Models;
+
+namespace ExamPrep.Repository.Interfaces
+{
+    public interface IUserRepository
+    {
+
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByUsernameAsync(string username);
+        Task<List<User>> GetAllUsersAsync();
+
+        // Note: The following methods are not part of the original code but are included for completeness.
+        Task<int> CreateAsync(User user);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> UpdateAsync(User user);
+    }
+}
